c07051e38340d48e06d8ab2fc2a59cc564b8aa79
Laurie Waller <lauriewaller7@gmail.com>
2021-08-07 12:18:52 -0700

    add event.target

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { v4 } from 'uuid';
 
-function NewTicketForm(props){
+function NewTicketForm(){
+
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}> 
@@ -21,6 +22,15 @@ function NewTicketForm(props){
       </form>
     </React.Fragment>
   );
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+  // event.target gives us access to the event that was just fired. In this case, we just had a submit event. We can actually grab the values that came from that submit event. 
 }
+
+
 
 export default NewTicketForm;
