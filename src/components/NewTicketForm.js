ea6567548b35ce44b9c0e44ff70720eeaea4e640
Laurie Waller <lauriewaller7@gmail.com>
2021-08-07 19:51:10 -0700

    create callback function to pass as prop

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types"; 
 
-function NewTicketForm(){
+function NewTicketForm(props){
 
   return (
     <React.Fragment>
@@ -24,6 +25,7 @@ function NewTicketForm(){
   );
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
     console.log(event.target.names.value);
     console.log(event.target.location.value);
     console.log(event.target.issue.value);
@@ -31,6 +33,8 @@ function NewTicketForm(){
   // event.target gives us access to the event that was just fired. In this case, we just had a submit event. We can actually grab the values that came from that submit event. 
 }
 
-
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Ticket(props){
+function Ticket(props){ 
   return (
     <React.Fragment>
       <h3>{props.location} - {props.names}</h3>
